80c58607ffb91e1df90241deeabe741c01891456
Rafael León <soylegendario@gmail.com>
2021-11-23 21:45:01 +0100

    Refactor and document methods

--- src/LSharp.DependencyInjection/InjectableAttribute.cs
+++ src/LSharp.DependencyInjection/InjectableAttribute.cs
@@ -6,8 +6,22 @@ namespace LSharp.DependencyInjection
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
     public class InjectableAttribute : Attribute
     {
+        /// <summary>
+        ///     Get the lifetime to inject the class
+        /// </summary>
         public ServiceLifetime ServiceLifetime { get; }
 
+        /// <summary>
+        ///     Define a injectable class with Transient lifetime
+        /// </summary>
+        public InjectableAttribute() : this(ServiceLifetime.Transient)
+        {
+        }
+
+        /// <summary>
+        ///     Define a injectable class
+        /// </summary>
+        /// <param name="serviceLifetime">Lifetime of the class</param>
         public InjectableAttribute(ServiceLifetime serviceLifetime)
         {
             ServiceLifetime = serviceLifetime;
--- src/LSharp.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/LSharp.DependencyInjection/ServiceCollectionExtensions.cs
@@ -7,74 +7,32 @@ namespace LSharp.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        ///     Configure the dependency injection for injectables classes 
+        /// </summary>
+        /// <param name="services">Service collection</param>
+        /// <returns></returns>
         public static IServiceCollection AddInjectables(this IServiceCollection services)
         {
             var injectables = GetTypesWithInjectableAttribute();
             foreach (var (injectableType, injectableAttribute) in injectables)
             {
-                var interfaces = injectableType.GetInterfaces();
-                Inject(services, injectableAttribute, injectableType, interfaces);
+                var interfaces = injectableType.GetInterfaces().FirstOrDefault() ?? injectableType;
+                services.Add(new ServiceDescriptor(interfaces, injectableType, injectableAttribute.ServiceLifetime));
             }
             return services;
         }
 
-        private static void Inject(IServiceCollection services, 
-            InjectableAttribute injectableAttribute, 
-            Type injectableType, 
-            IEnumerable<Type> interfaces)
-        {
-            switch (injectableAttribute.ServiceLifetime)
-            {
-                case ServiceLifetime.Singleton:
-                    if (!interfaces.Any())
-                    {
-                        services.AddSingleton(injectableType);
-                    } else
-                    {
-                        foreach (var serviceType in interfaces)
-                        {
-                            services.AddSingleton(serviceType, injectableType);
-                        }
-                    }
-                    break;
-                case ServiceLifetime.Scoped:
-                    if (!interfaces.Any())
-                    {
-                        services.AddScoped(injectableType);
-                    }
-                    else
-                    {
-                        foreach (var serviceType in interfaces)
-                        {
-                            services.AddScoped(serviceType, injectableType);
-                        }
-                    }
-                    break;
-                case ServiceLifetime.Transient:
-                    if (!interfaces.Any())
-                    {
-                        services.AddTransient(injectableType);
-                    }
-                    else
-                    {
-                        foreach (var serviceType in interfaces)
-                        {
-                            services.AddTransient(serviceType, injectableType);
-                        }
-                    }
-                    break;
-                default:
-                    throw new Exception("Cannot configure injectable services");
-            }
-        }
-
+        /// <summary>
+        ///     Get the all classes decorated with <see cref="InjectableAttribute"/> in all assemblies
+        /// </summary>
         private static IEnumerable<Tuple<Type, InjectableAttribute>> GetTypesWithInjectableAttribute()
         {
             var injectableTypes =
                 from a in AppDomain.CurrentDomain.GetAssemblies().AsParallel()
                 from t in a.GetTypes()
                 let attributes = t.GetCustomAttributes(typeof(InjectableAttribute), true)
-                where attributes != null && attributes.Length > 0
+                where attributes is { Length: > 0 }
                 select new { Type = t, InjectableAttribute = attributes.Cast<InjectableAttribute>().First() };
             foreach (var o in injectableTypes)
             {
